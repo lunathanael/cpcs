5fcbe6aceea83c9f574a50519728c5e24f96a5a7
Nathanael Lu <lunathanael@gmail.com>
2023-05-11 09:11:36 -0400

    FEB_2023

---
+++ USACO/Solutions/FEB_2023.cpp
@@ -0,0 +1,166 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+string conversation;
+int conversationLength;
+
+int excitement = 0;
+vector<int> poss;
+vector<int> output;
+
+// Possiblities
+// NO fs
+// 111 101 001 011
+//  2   0   1   1
+// Sender F only
+// 1F1  0F1
+// 2,0   1
+// double F
+// FF1     1FF
+// 0,1,2 + count, end   0,1, 2, count + 1
+// triple F
+// FFF
+// 0,1,2, count, poss end
+
+
+// FF1 Tag 1, end and push, 1FF tag 1, push not end.
+
+
+// First
+// 11x  10x
+// +1    +0
+// Then
+// 1f1  1f0  1ff
+// 2,0   +1   +1, c1
+// ff1
+// 1, +c
+
+
+
+void GenerateLevels() {
+	sort(poss.begin(), poss.end());
+	if (poss.size() == 0) {
+		output.push_back(excitement);
+		return;
+	}
+	bool canCount = (poss[0] == 1);
+	output.push_back(excitement);
+	if (canCount) {
+		int i = 0;
+		while (poss[i] == 1) {
+			++excitement;
+			output.push_back(excitement);
+			++i;
+		}
+		while (i < poss.size()) {
+			++excitement;
+			output.push_back(excitement);
+			++excitement;
+			output.push_back(excitement);
+			++i;
+		}
+	}
+	else {
+		for (int i = 0; i < poss.size(); ++i) {
+			excitement += 2;
+			output.push_back(excitement);
+		}
+	}
+
+}
+
+int main() {
+	cin >> conversationLength;
+	cin >> conversation;
+	int counter = 0;
+
+	char chainStart;
+	char sender, prevSender, nextSender;
+
+
+	sender = conversation.at(0);
+
+	if (sender == 'F') {
+		chainStart = 'N';
+	}
+
+	for (int i = 1; i < conversationLength; ++i) {
+
+		prevSender = conversation.at(i - 1);
+		sender = conversation.at(i);
+
+		if (sender != 'F') {
+			if (sender == prevSender) {
+				++excitement;
+			}
+
+
+			else if (prevSender == 'F') {
+
+
+
+				if (chainStart == 'N') {
+					poss.push_back(1);
+					continue;
+				}
+				else {
+					if (counter % 2 == 0) {
+						if (sender == chainStart) {
+							++excitement;
+						}
+						counter = counter / 2;
+
+					}
+					else {
+						if (sender != chainStart) {
+							++excitement;
+							counter = (counter - 1) / 2;
+						}
+						else {
+							counter = (counter + 1) / 2;
+						}
+					}
+					for (int i = 0; i < counter; ++ i) {
+						poss.push_back(2);
+					}
+				}
+			}
+			counter = 0;
+		}
+		else {  //sender == "F"
+			++counter;
+			if (prevSender != 'F') {
+				chainStart = prevSender;
+			}
+			
+		}
+
+	}
+
+	if (sender == 'F') {
+		for (int i = 0; i < counter; ++i) {
+			poss.push_back(1);
+		}
+	}
+
+
+	GenerateLevels();
+
+	cout << output.size() << endl;
+	for (int i = 0; i < output.size(); ++i) {
+		cout << output[i] << endl;
+	}
+
+
+	return 0;
+}
+
+
+
+
+

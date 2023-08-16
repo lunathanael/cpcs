85f9f5d0ecc3cb208f9e35ccca299131ed684bb1
lunathanael <68858103+lunathanael@users.noreply.github.com>
2023-08-15 22:12:10 -0400

    new

--- USACO/Milk_Sum_2023.cpp
+++ USACO/Milk_Sum_2023.cpp
@@ -1,36 +1,89 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 
-long long int calculate(int arr[], int n) {
-	long long int total = 0;
-	for (long long int i = 0; i < n;) {
-		total += (long long int)((long long int)arr[i++] * (long long int)(i));
-	}
-	return total;
-}
-
 int main() {
-	int N;
-	cin >> N;
+	ios_base::sync_with_stdio(false); cin.tie(0);
 
-	int cows[N];
-	for (int i = 0; i < N; ++i) {
-		cin >> cows[i];
+	unsigned long long int N;
+	cin >> N;
+	++N;
+
+	unsigned long long int original_order[N] = { 0 };
+	unsigned long long int milk_index[N];
+	milk_index[0] = 0;
+	for (int i = 1; i < N; i++) {
+		unsigned long long int value;
+		cin >> value;
+		original_order[i] = value;
+		milk_index[i] = value;
+	}
+	sort(milk_index, milk_index + N);
+
+	unsigned long long int initial_total = 0;
+	for (unsigned long long int i = 1; i < N; ++i) {
+		initial_total += (unsigned long long)(milk_index[i] * i);
 	}
 
+	
 	int Q;
 	cin >> Q;
 	for (int a = 0; a < Q; ++a) {
-		int temp[N];
-		copy(cows, cows + N, temp);
-		int i, j;
-		cin >> i >> j;
-		temp[i-1] = j;
-		sort(temp, temp+N);
-		cout << calculate(temp, N) << "\n";
+		// find replaced, see difference
+		unsigned long long int new_total = initial_total;
+		unsigned long long int i, new_value;
+		cin >> i >> new_value;
+		unsigned long long int original_value = original_order[i];
+
+		// find direction
+		if (new_value > original_value) {
+
+			//find location
+			unsigned long long int x = 1;
+			for (; x < N; ++x) {
+				if (milk_index[x] == original_value) {
+					break;
+				}
+			}
+
+			new_total -= (unsigned long long)(original_value * x);
+			++x;
+			while (x < N) {
+				if (new_value > milk_index[x]) {
+					new_total -= milk_index[x];
+					++x;
+					continue;
+				}
+				break;
+			}
+			--x;
+			new_total += (unsigned long long)(new_value * x);
+		}
+		else if (new_value < original_value) {
+
+			//find location
+			unsigned long long int x = 1;
+			for (; x < N; ++x) {
+				if (milk_index[x] == original_value) {
+					break;
+				}
+			}
+
+			new_total -= (unsigned long long)(original_value * x);
+			--x;
+			while (x > 0) {
+				if (new_value < milk_index[x]) {
+					new_total += milk_index[x];
+					--x;
+					continue;
+				}
+				break;
+			}
+			++x;
+			new_total += (unsigned long long)(new_value * x);
+		}
+		cout << new_total << "\n";
 	}
 
 	return 0;

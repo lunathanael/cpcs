640f307885039da6732f2bd2dd3d469ed175251f
Nathanael Lu <lunathanael@gmail.com>
2024-12-02 23:59:26 -0500

    DEV: Prep day3

--- aoc/lib/segment_tree.hpp
+++ aoc/lib/segment_tree.hpp
@@ -49,90 +49,90 @@ template <typename F, typename T = int> struct SegTree {
   }
 };
 
-/**
- * Author: Simon Lindholm
- * License: CC0
- * Description: Segment tree to add or set values of large intervals.
- * Use bump allocator for perf, and SmallPtr or implicit indices for memory.
- * Time: O(log N).
- * Usage: Node* tr = new Node(v, 0, sz(v));
- */
-// #include "../various/BumpAllocator.h"
-const int inf = 1e9;
-struct Node {
-  Node *l = nullptr, *r = nullptr;
-  int lo, hi, mset = inf, madd = 0, val = -inf;
-  Node(int lo, int hi) : lo(lo), hi(hi) {} // Large interval of -inf
-  ~Node() {
-    delete l;
-    delete r;
-  }
-  Node(vector<int> &v, int lo, int hi) : lo(lo), hi(hi) {
-    if (lo + 1 < hi) {
-      int mid = lo + (hi - lo) / 2;
-      l = new Node(v, lo, mid);
-      r = new Node(v, mid, hi);
-      // val = max(l->val, r->val);
-      val = l->val + r->val;
-    } else
-      val = v[lo];
-  }
-  int query(int L, int R) {
-    if (R <= lo || hi <= L)
-      return 0;
-    if (L <= lo && hi <= R)
-      return val;
-    push();
+// /**
+//  * Author: Simon Lindholm
+//  * License: CC0
+//  * Description: Segment tree to add or set values of large intervals.
+//  * Use bump allocator for perf, and SmallPtr or implicit indices for memory.
+//  * Time: O(log N).
+//  * Usage: Node* tr = new Node(v, 0, sz(v));
+//  */
+// // #include "../various/BumpAllocator.h"
+// const int inf = 1e9;
+// struct Node {
+//   Node *l = nullptr, *r = nullptr;
+//   int lo, hi, mset = inf, madd = 0, val = -inf;
+//   Node(int lo, int hi) : lo(lo), hi(hi) {} // Large interval of -inf
+//   ~Node() {
+//     delete l;
+//     delete r;
+//   }
+//   Node(vector<int> &v, int lo, int hi) : lo(lo), hi(hi) {
+//     if (lo + 1 < hi) {
+//       int mid = lo + (hi - lo) / 2;
+//       l = new Node(v, lo, mid);
+//       r = new Node(v, mid, hi);
+//       // val = max(l->val, r->val);
+//       val = l->val + r->val;
+//     } else
+//       val = v[lo];
+//   }
+//   int query(int L, int R) {
+//     if (R <= lo || hi <= L)
+//       return 0;
+//     if (L <= lo && hi <= R)
+//       return val;
+//     push();
 
-    // return max(l->query(L, R), r->query(L, R));
-    return l->query(L, R) + r->query(L, R);
-  }
-  void set(int L, int R, int x) {
-    if (R <= lo || hi <= L)
-      return;
-    if (L <= lo && hi <= R)
-      mset = val = x, madd = 0;
-    else {
-      push(), l->set(L, R, x), r->set(L, R, x);
-      // val = max(l->val, r->val);
-      val = l->val + r->val;
-    }
-  }
-  void add(int L, int R, int x) {
-    if (R <= lo || hi <= L)
-      return;
-    if (L <= lo && hi <= R) {
-      if (mset != inf)
-        mset += x;
-      else
-        madd += x;
-      val += x;
-    } else {
-      push(), l->add(L, R, x), r->add(L, R, x);
-      // val = max(l->val, r->val);
-      val = l->val + r->val;
-    }
-  }
-  void push() {
-    if (!l) {
-      int mid = lo + (hi - lo) / 2;
-      l = new Node(lo, mid);
-      r = new Node(mid, hi);
-    }
-    if (mset != inf)
-      l->set(lo, hi, mset), r->set(lo, hi, mset), mset = inf;
-    else if (madd)
-      l->add(lo, hi, madd), r->add(lo, hi, madd), madd = 0;
-  }
-};
+//     // return max(l->query(L, R), r->query(L, R));
+//     return l->query(L, R) + r->query(L, R);
+//   }
+//   void set(int L, int R, int x) {
+//     if (R <= lo || hi <= L)
+//       return;
+//     if (L <= lo && hi <= R)
+//       mset = val = x, madd = 0;
+//     else {
+//       push(), l->set(L, R, x), r->set(L, R, x);
+//       // val = max(l->val, r->val);
+//       val = l->val + r->val;
+//     }
+//   }
+//   void add(int L, int R, int x) {
+//     if (R <= lo || hi <= L)
+//       return;
+//     if (L <= lo && hi <= R) {
+//       if (mset != inf)
+//         mset += x;
+//       else
+//         madd += x;
+//       val += x;
+//     } else {
+//       push(), l->add(L, R, x), r->add(L, R, x);
+//       // val = max(l->val, r->val);
+//       val = l->val + r->val;
+//     }
+//   }
+//   void push() {
+//     if (!l) {
+//       int mid = lo + (hi - lo) / 2;
+//       l = new Node(lo, mid);
+//       r = new Node(mid, hi);
+//     }
+//     if (mset != inf)
+//       l->set(lo, hi, mset), r->set(lo, hi, mset), mset = inf;
+//     else if (madd)
+//       l->add(lo, hi, madd), r->add(lo, hi, madd), madd = 0;
+//   }
+// };
 
-struct LazySegTree {
-  Node *root;
-  LazySegTree(vector<int> &v) : root(new Node(v, 0, v.size())) {}
-  ~LazySegTree() { delete root; }
-  int query(int L, int R) { return root->query(L, R); }
-  void set(int L, int R, int x) { root->set(L, R, x); }
-  void add(int L, int R, int x) { root->add(L, R, x); }
-};
+// struct LazySegTree {
+//   Node *root;
+//   LazySegTree(vector<int> &v) : root(new Node(v, 0, v.size())) {}
+//   ~LazySegTree() { delete root; }
+//   int query(int L, int R) { return root->query(L, R); }
+//   void set(int L, int R, int x) { root->set(L, R, x); }
+//   void add(int L, int R, int x) { root->add(L, R, x); }
+// };
 
 #endif
--- aoc/main.cpp
+++ aoc/main.cpp
@@ -44,29 +44,19 @@ struct hash_pair {
   }
 };
 
-// Create a segment tree that performs addition
-BINOP(add, a + b);
+void process() { auto in = read_file("in.txt"); }
 
+void part1() {}
+
+void part2() {}
 
 int main() {
   IOSOPT;
+  // Start here
+  process();
 
-  // LazySegTree<add<int>, int> seg(10, 0); // Size 10, initialized with 0
-  // Test basic operations
-  vector<int> v = {0, 0, 5, 0, 3, 0, 0, 2};
-  LazySegTree seg(v);
-
-  // Test range queries
-  cout << "Sum [0,4]: " << seg.query(0, 4 + 1) << "\n"; // Should be 8 (5 + 3)
-  cout << "Sum [2,7]: " << seg.query(2, 7 + 1)
-       << "\n"; // Should be 10 (5 + 3 + 2)
-  cout << "Sum [0,9]: " << seg.query(0, 9 + 1)
-       << "\n"; // Should be 10 (total sum)
-
-  // Test single point update and query
-  seg.set(2, 2, 1); // Change position 2 from 5 to 1
-  cout << "After update, Sum [0,4]: " << seg.query(0, 4)
-       << "\n"; // Should be 4 (1 + 3)
+  part1();
+  part2();
 
   return 0;
 }
